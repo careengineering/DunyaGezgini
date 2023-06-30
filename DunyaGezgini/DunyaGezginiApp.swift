197c9da396f6adbc9877e367f47e39114271ee5e
cesurkedi <cesurkedixx@protonmail.com>
2023-06-30 17:53:48 +0300

    Initial Commit

---
+++ DunyaGezgini/DunyaGezginiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DunyaGezginiApp.swift
+//  DunyaGezgini
+//
+//  Created by Çare C. on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DunyaGezginiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
